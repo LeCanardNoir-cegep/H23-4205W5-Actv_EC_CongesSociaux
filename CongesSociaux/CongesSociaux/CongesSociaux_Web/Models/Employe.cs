16f6834b1b01ebb4deb5ecaec48ab4dfb28ed59a
Bruno Pouliot <2168516@cegepmontpetit.ca>
2023-04-27 17:47:39 -0400

    PROGRESS: Ajout des modèles.

---
+++ CongesSociaux/CongesSociaux/CongesSociaux_Web/Models/Employe.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CongesSociaux_Web.Models
+{
+    public abstract class Employe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Prenom { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nom { get; set; }
+
+        [Required]
+        public DateTime DateEmbauche { get; set; }
+
+    }
+}
