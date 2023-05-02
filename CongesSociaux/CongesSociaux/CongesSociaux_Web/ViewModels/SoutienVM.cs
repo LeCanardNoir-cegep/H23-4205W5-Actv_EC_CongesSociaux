bb28ca8a1d87efcb4bb8ff9a74afc453f5b6c8e0
2156153 <m.gagnon941@gmail.com>
2023-05-02 10:02:52 -0400

    FCT: Create soutien viewmodel

---
+++ CongesSociaux/CongesSociaux/CongesSociaux_Web/ViewModels/SoutienVM.cs
@@ -0,0 +1,14 @@
+﻿using CongesSociaux_Web.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace CongesSociaux_Web.ViewModels
+{
+	public class SoutienVM
+	{
+		public int Id { get; set; }
+		public string? Prenom { get; set; }
+		public string? Nom { get; set; }
+		public DateTime DateEmbauche { get; set; }
+		public string? Poste { get; set; }
+	}
+}
